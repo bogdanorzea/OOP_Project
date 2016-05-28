cd63c46b96bf2771f7c3f751361e640224b0ab19
Bogdan Orzea <bogdan.orzea@gmail.com>
2016-05-28 09:51:58 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "stdio"
+#include "string"
+
+struct MyStruct
+{
+  int i;
+  int b;
+  inc d;
+};
+
+int main(){
+  MyStruct ana;
+  ana.d = 5;
+  
+  int i=6;
+  string a = "Mama";
+  cout >> i;
+  
+}
